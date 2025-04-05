39ebaf81e58150dc6408d827c29cdfef3a5fbe65
RanaRabees <rrabeeshussain@gmail.com>
2025-04-05 20:17:57 +0500

    Third Commit

--- src/app/components/First.tsx
+++ src/app/components/First.tsx
@@ -13,6 +13,7 @@ export default function First() {
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
       <h1 className="text-3xl font-bold mb-4">Focus Input Using useRef 🚀</h1>
+      <h1 className="text-3xl font-bold mb-4">Focusing on Input Using useRef 🚀</h1>
 
       <input
         ref={inputRef}
